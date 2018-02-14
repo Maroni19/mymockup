8f57b0238ade72a9a716752692a16d526a62c4f7
Maroni19 <maroni101010@gmail.com>
2018-02-14 14:35:12 +0200

    create first section

--- js/build.js
+++ js/build.js
@@ -1,2 +1,8 @@
+$(function () {
+    console.log('ham');
+    $('._hamburger').click(function () {
+        $('._nav').slideToggle();
+    });
+});
 console.log("start module1");
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1vZHVsZTEuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoiYnVpbGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcInN0YXJ0IG1vZHVsZTFcIik7Il19
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImhhbWJ1cmdlci5qcyIsIm1vZHVsZTEuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FDTEEiLCJmaWxlIjoiYnVpbGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGZ1bmN0aW9uICgpIHtcclxuICAgIGNvbnNvbGUubG9nKCdoYW0nKTtcclxuICAgICQoJy5faGFtYnVyZ2VyJykuY2xpY2soZnVuY3Rpb24gKCkge1xyXG4gICAgICAgICQoJy5fbmF2Jykuc2xpZGVUb2dnbGUoKTtcclxuICAgIH0pO1xyXG59KTsiLCJjb25zb2xlLmxvZyhcInN0YXJ0IG1vZHVsZTFcIik7Il19
